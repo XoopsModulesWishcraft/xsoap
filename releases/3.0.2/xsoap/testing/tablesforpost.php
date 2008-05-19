9af07d32a3a844581dea2edb4431dae4052d7f6a
wishcraft <wishcraft@c17157d5-08f2-41e7-a131-df055f6a0da3>
2008-05-19 12:20:23 +0000

    X-Soap 3.0.2 - archives/xoops2_xsoap_3.0.2.zip, Source, Trunk Updated.

---
+++ releases/3.0.2/xsoap/testing/tablesforpost.php
@@ -0,0 +1,19 @@
+<?php
+// Pull in the NuSOAP code
+require_once('../include/nusoap/nusoap.php');
+// Create the client instance
+$client = new soapclient('http://www.unseen.org.au/modules/xsoap/');
+// Call the SOAP method
+$result = $client->call('tablesforpost', array("xsoap" => array('username' => 'test_acc', "password" => "test")));
+// Display the result
+print_r($result);
+// Display the request and response
+echo '<h2>Request</h2>';
+echo '<pre>' . htmlspecialchars($client->request, ENT_QUOTES) . '</pre>';
+echo '<h2>Response</h2>';
+echo '<pre>' . htmlspecialchars($client->response, ENT_QUOTES) . '</pre>';
+// Display the debug messages
+echo '<h2>Debug</h2>';
+echo '<pre>' . htmlspecialchars($client->debug_str, ENT_QUOTES) . '</pre>';
+
+?>
