026784b88311138ae4d2615ddcf1e0284def4ab9
wishcraft <wishcraft@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-06-03 12:11:07 +0000

    X-SOAP 4.2 - Release (Final)

---
+++ releases/4.2/xsoap/admin/menu.php
@@ -0,0 +1,15 @@
+<?php
+
+global $adminmenu;
+$adminmenu[1]['title'] = _XS_ADMINMENU_1;
+$adminmenu[1]['link'] = "admin/index.php?op=tables";
+$adminmenu[2]['title'] = _XS_ADMINMENU_2;
+$adminmenu[2]['link'] = "admin/index.php?op=fields";
+$adminmenu[3]['title'] = _XS_ADMINMENU_3;
+$adminmenu[3]['link'] = "admin/index.php?op=views";
+$adminmenu[4]['title'] = _XS_ADMINMENU_4;
+$adminmenu[4]['link'] = "admin/index.php?op=plugins";
+$adminmenu[5]['title'] = _XS_ADMINMENU_5;
+$adminmenu[5]['link'] = "admin/permissions.php";
+
+?>
