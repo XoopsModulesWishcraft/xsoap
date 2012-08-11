1db9b4568d1b68172ed55a25d6b1876b56565eaa
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-11 12:46:10 +0000

    Removing spaces from "define", which is required for Transifex per DCrusader

--- releases/4.40/htdocs/module/xsoap/language/english/admin.php
+++ releases/4.40/htdocs/module/xsoap/language/english/admin.php
@@ -1,12 +1,12 @@
 <?php
 
-	define('_XS_PLUGINAVAILABLE', 'SOAP Plugins Available');
-	define('_XS_PLUGINAVAILABLE', 'SOAP Plugins Available');
-	define('_XS_SELECTTABLE', 'Please Select Table to Work with');
-	define('_XS_TABLESAVAILABLE', 'Tables available in database');
-	define('_XS_FIELDOPTIONSFOR', 'Field options within the table selected');	
-	define('_XS_VIEWSFOR', 'Views available in database');
-	define('_XS_WSDLSUCCESSFUL', 'Writting WDSL Successful');
-	define('_XS_WSDLUNSUCCESSFUL', 'Writting WDSL was Unsuccessful<br/>Please check the permissioning of the xsoap folder and make sure it is writable');
+	define('_XS_PLUGINAVAILABLE','SOAP Plugins Available');
+	define('_XS_PLUGINAVAILABLE','SOAP Plugins Available');
+	define('_XS_SELECTTABLE','Please Select Table to Work with');
+	define('_XS_TABLESAVAILABLE','Tables available in database');
+	define('_XS_FIELDOPTIONSFOR','Field options within the table selected');	
+	define('_XS_VIEWSFOR','Views available in database');
+	define('_XS_WSDLSUCCESSFUL','Writting WDSL Successful');
+	define('_XS_WSDLUNSUCCESSFUL','Writting WDSL was Unsuccessful<br/>Please check the permissioning of the xsoap folder and make sure it is writable');
 
 ?>
--- releases/4.40/htdocs/module/xsoap/language/english/modinfo.php
+++ releases/4.40/htdocs/module/xsoap/language/english/modinfo.php
@@ -24,21 +24,21 @@
 	define('_XS_WSDLUNSUCCESSFUL','Database Updated - Error Compiling WSDL');
 	define('_XS_DATABASEUPDATED','Database Updated');						
 	
-	define('_XS_SECONDS', 'Function lock out time');
-	define('_XS_SECONDS_DESC', 'Period of time for locked function to be invocated on incorrect username and password details!!');
+	define('_XS_SECONDS','Function lock out time');
+	define('_XS_SECONDS_DESC','Period of time for locked function to be invocated on incorrect username and password details!!');
 
-	define('_XS_SECONDSCACHE', 'Lockout cache stored for');
-	define('_XS_SECONDSCACHE_DESC', 'Period of time for cache function to be invocated!');
+	define('_XS_SECONDSCACHE','Lockout cache stored for');
+	define('_XS_SECONDSCACHE_DESC','Period of time for cache function to be invocated!');
 		
-	define('_XS_SECONDS_3600', '1 Hour');
-	define('_XS_SECONDS_1800', '30 minutes');
-	define('_XS_SECONDS_1200', '20 minutes');
-	define('_XS_SECONDS_600', '10 minutes');
-	define('_XS_SECONDS_300', '5 Minutes');
-	define('_XS_SECONDS_180', '3 Minutes');
-	define('_XS_SECONDS_60', '1 Minute');
-	define('_XS_SECONDS_30', '30 Seconds');	
+	define('_XS_SECONDS_3600','1 Hour');
+	define('_XS_SECONDS_1800','30 minutes');
+	define('_XS_SECONDS_1200','20 minutes');
+	define('_XS_SECONDS_600','10 minutes');
+	define('_XS_SECONDS_300','5 Minutes');
+	define('_XS_SECONDS_180','3 Minutes');
+	define('_XS_SECONDS_60','1 Minute');
+	define('_XS_SECONDS_30','30 Seconds');	
 	
-	define('_XS_USERANDOMLOCK', 'Random seconds seed (maximum value)');
-	define('_XS_USERANDOMLOCK_DESC', 'You should not set this above the maximum function lockout time!');
+	define('_XS_USERANDOMLOCK','Random seconds seed (maximum value)');
+	define('_XS_USERANDOMLOCK_DESC','You should not set this above the maximum function lockout time!');
 ?>
